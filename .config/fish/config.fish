234797e37f61bf8bb3021a750771039e23f0a1f5
shans10 <shantanu.shukla10@gmail.com>
2021-01-19 23:55:24 +0530

    added XDG_DATA_DIRS variable in fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,79 +11,8 @@ end
 
 ### PROMPT ###
 
-# This was the 'sashimi' prompt from oh-my-fish.
-function fish_prompt
-  set -l last_status $status
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -g red (set_color -o red)
-  set -g blue (set_color -o blue)
-  set -l green (set_color -o green)
-  set -g normal (set_color normal)
-
-  set -l ahead (_git_ahead)
-  set -g whitespace ' '
-
-  if test $last_status = 0
-    set initial_indicator "$green◆"
-    set status_indicator "$normal❯$cyan❯$green❯"
-  else
-    set initial_indicator "$red✖ $last_status"
-    set status_indicator "$red❯$red❯$red❯"
-  end
-  set -l cwd $cyan(basename (prompt_pwd))
-
-  if [ (_git_branch_name) ]
-
-    if test (_git_branch_name) = 'master'
-      set -l git_branch (_git_branch_name)
-      set git_info "$normal git:($red$git_branch$normal)"
-    else
-      set -l git_branch (_git_branch_name)
-      set git_info "$normal git:($blue$git_branch$normal)"
-    end
-
-    if [ (_is_git_dirty) ]
-      set -l dirty "$yellow ✗"
-      set git_info "$git_info$dirty"
-    end
-  end
-
-  # Notify if a command took more than 5 minutes
-  if [ "$CMD_DURATION" -gt 300000 ]
-    echo The last command took (math "$CMD_DURATION/1000") seconds.
-  end
-
-  echo -n -s $initial_indicator $whitespace $cwd $git_info $whitespace $ahead $status_indicator $whitespace
-end
-
-function _git_ahead
-  set -l commits (command git rev-list --left-right '@{upstream}...HEAD' ^/dev/null)
-  if [ $status != 0 ]
-    return
-  end
-  set -l behind (count (for arg in $commits; echo $arg; end | grep '^<'))
-  set -l ahead  (count (for arg in $commits; echo $arg; end | grep -v '^<'))
-  switch "$ahead $behind"
-    case ''     # no upstream
-    case '0 0'  # equal to upstream
-      return
-    case '* 0'  # ahead of upstream
-      echo "$blue↑$normal_c$ahead$whitespace"
-    case '0 *'  # behind upstream
-      echo "$red↓$normal_c$behind$whitespace"
-    case '*'    # diverged from upstream
-      echo "$blue↑$normal$ahead $red↓$normal_c$behind$whitespace"
-  end
-end
-
-function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-end
+# Starship PROMPT
+starship init fish | source
 
 
 ### FUNCTIONS ###
@@ -191,9 +120,21 @@ alias initramfs-gen='sudo mkinitcpio -p linux'
 
 ### SETTING FISH ENVIRONMENT VARIABLES ###
 
-set TERM "xterm-256color"              # Sets the terminal type
+### SETTING FISH ENVIRONMENT VARIABLES ###
+
+set TERM "xterm-256color"           # Sets the terminal type
 set EDITOR "alacritty -e nvim"      # $EDITOR use Emacs in terminal
-set VISUAL "emacs"   # $VISUAL use Emacs in GUI mode
+set VISUAL "nvim-qt"                # $VISUAL use Neovim-Qt
+
+# Flatpak XDG_DATA_DIRS Workaround
+set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
+set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+
+for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    if test -d $flatpakdir
+        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
+    end
+end
 
 # Set Locale
 export LANG="en_US.UTF-8"
@@ -204,9 +145,6 @@ export LC_ALL="en_US.UTF-8"
 export CC="clang"
 export CFLAGS="-fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow"
 export LDLIBS="-lcrypt -lcs50 -lm"
-
-# 256 colors
-export TERM="xterm-256color"
 
 # QT Platform
 export QT_QPA_PLATFORMTHEME=qt5ct
